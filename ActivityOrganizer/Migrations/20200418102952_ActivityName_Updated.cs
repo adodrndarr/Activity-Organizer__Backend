349d2d84ebfb78e3e2df368340b5df2e5f2f4ca9
Ado <drndarr@gmail.com>
2020-07-10 20:00:35 +0200

    update

---
+++ ActivityOrganizer/Migrations/20200418102952_ActivityName_Updated.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ActivityOrganizer.Migrations
+{
+    public partial class ActivityName_Updated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ActivityName",
+                table: "SpecialActivity",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ActivityName",
+                table: "SpecialActivity",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 40);
+        }
+    }
+}
---
+++ ActivityOrganizer/Models/ActivityWithPriority.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace ActivityOrganizer.Models
+{
+    public class ActivityWithPriority
+    {
+        public List<SpecialActivity> Activities { get; set; }
+
+        public SelectList Priorities { get; set; }
+
+        public string Priority { get; set; }
+    }
+}
